16980497bed9fb92c3c9defdec2e21c2485de7ae
520wsl <395548460@qq.com>
2019-08-02 19:56:39 +0800

    目录 文章内容 获取

--- scrapy_novel_spider/settings.py
+++ scrapy_novel_spider/settings.py
@@ -26,7 +26,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 5
+DOWNLOAD_DELAY = 10
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -99,7 +99,7 @@ DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'
 
 # 设置 redis 为 item pipeline
 ITEM_PIPELINES = {
-    'scrapy_redis.pipelines.RedisPipeline': 300,
+    # 'scrapy_redis.pipelines.RedisPipeline': 300,
     'scrapy_novel_spider.pipelines.ScrapyNovelSpiderTwistedPipeline': 301,
     # 'scrapy_novel_spider.pipelines.ScrapyNovelSpiderPipeline': 301,
 }
